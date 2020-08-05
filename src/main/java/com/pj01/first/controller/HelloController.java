d70c3ff246636017ae0e1e8c0ccbb3baa903ae22
谭子贤 <936240187@qq.com>
2020-08-05 10:59:45 +0800

    Initial commit

---
+++ src/main/java/com/pj01/first/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.pj01.first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/test/java/com/pj01/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj01.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
